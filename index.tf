aba64a39457b98020b9502d70db082077e5cca3d
krish <krish@90>
2024-04-09 03:37:48 +0530

    modified terraform file

--- index.tf
+++ index.tf
@@ -5,7 +5,10 @@ region = "ap-south-1"
   
 resource "aws_instance" example" {  
    
-ami = "ami-3bb6d83c60fc5f7c"  
+ami = "ami-03bb6d83c60fc5f7c"  
    
-instance_type = "t2.micro"  
+instance_type = "t2.micro" 
+tags = {
+    name = "krishna"
+ }
 }  
